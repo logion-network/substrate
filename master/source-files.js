82733e656572c0b2b44cd03437d8d94b866bfb51
paritytech-ci <devops-team@parity.io>
2022-06-14 04:37:23 +0000

    ___Updated docs for master___

--- master/source-files.js
+++ master/source-files.js
@@ -393,6 +393,7 @@ sourcesIndex["pallet_nicks"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["pallet_node_authorization"] = {"name":"","files":["lib.rs","weights.rs"]};
 sourcesIndex["pallet_nomination_pools"] = {"name":"","files":["lib.rs","migration.rs","weights.rs"]};
 sourcesIndex["pallet_nomination_pools_benchmarking"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["pallet_nomination_pools_test_staking"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["pallet_offences"] = {"name":"","files":["lib.rs","migration.rs"]};
 sourcesIndex["pallet_offences_benchmarking"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["pallet_preimage"] = {"name":"","files":["benchmarking.rs","lib.rs","weights.rs"]};
